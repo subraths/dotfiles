d40cecd0a2ed89acb9bca45713bf393d5e09ea71
Subrath S <subrath0626@gmail.com>
2025-08-19 20:52:06 +0530

    update wifi script

--- hypr/scripts/connect-wifi.sh
+++ hypr/scripts/connect-wifi.sh
@@ -17,13 +17,20 @@ sleep 1
 
 # search for known devices
 
+connected = 0
 for network in "${known_devices[@]}"; do
   if iwctl station wlan0 get-networks | grep -q "$network"; then
     iwctl station wlan0 connect "$network"
     notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Connecting to $network"
     sleep 1
     connected_network=$(iwctl station wlan0 get-networks | grep -i ">" | awk '{print $4}')
+    connected=1
     notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "Connected to $connected_network"
     break
   fi
 done
+
+if [ $connected -eq 0 ]; then
+  notify-send -h string:x-canonical-private-synchronous:sys-notify -u low -i "$(get_icon)" "No known networks found"
+fi
+
